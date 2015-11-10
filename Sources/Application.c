ba400399e9311b5aa4e003158330c5eb77bcbea6
Kilian Ineichen <kilian.ineichen@stud.hslu.ch>
2015-11-10 14:12:25 +0100

    ultraschall sensor working

--- Sources/Application.c
+++ Sources/Application.c
@@ -8,30 +8,13 @@
 #include "Shell.h"
 #include "Infrarot.h"
 
-static portTASK_FUNCTION(Task1, pvParameters) {
-  (void)pvParameters; /* parameter not used */
-  for(;;) {
-    //LED1_Neg();
-    FRTOS1_vTaskDelay(1000/portTICK_RATE_MS);
-  }
-}
+
 
 void APP_Run(void) {
   SHELL_Init();
   US_Init();
   INF_Init();
-  if (FRTOS1_xTaskCreate(
-        Task1,  /* pointer to the task */
-        "Task1", /* task name for kernel awareness debugging */
-        configMINIMAL_STACK_SIZE, /* task stack size */
-        (void*)NULL, /* optional task startup argument */
-        tskIDLE_PRIORITY,  /* initial priority */
-        (xTaskHandle*)NULL /* optional task handle to create */
-      ) != pdPASS) {
-    /*lint -e527 */
-    for(;;){}; /* error! probably out of memory */
-    /*lint +e527 */
-  }
+
   FRTOS1_vTaskStartScheduler();
 }
 
